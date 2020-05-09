5efbc847655eb71a8e5f9e0a661e0afb374dc048
Franco Catrin <fcatrin@gmail.com>
2020-05-09 02:54:07 -0300

    Fixes JNI library build

--- drivers/gl/src/fts/gl/GLNativeInterface.java
+++ drivers/gl/src/fts/gl/GLNativeInterface.java
@@ -2,6 +2,10 @@ package fts.gl;
 
 public class GLNativeInterface {
 	
+	static {
+		System.loadLibrary("fts-gl");
+	}
+	
 	public static native void frameStart(int width, int height);
 	public static native void frameEnd();
 
