26c75e3ab5cb8e4d5cfdcf9582aae48aeb440adb
Franco Catrin <fcatrin@gmail.com>
2020-05-08 21:36:31 -0300

    Move stat / filter code into SelectorDrawable

--- core/src/fts/graphics/Drawable.java
+++ core/src/fts/graphics/Drawable.java
@@ -3,7 +3,6 @@ package fts.graphics;
 import fts.core.Component;
 
 public abstract class Drawable extends Component {
-	private boolean state[] = new boolean[10];
 	protected Rectangle padding = new Rectangle();
 	protected Rectangle bounds = new Rectangle();
 	
@@ -15,36 +14,6 @@ public abstract class Drawable extends Component {
 		this.padding = padding.clone();
 	}
 
-	public void setState(int index, boolean value) {
-		state[index] = value;
-	}
-	
-	protected String createFilterMask() {
-		String mask = "";
-		for (boolean stateValue : state) {
-			mask = mask + (stateValue?"1":0);
-		}
-		return mask;
-	}
-	
-	protected boolean filterMatch(String filter, String mask) {
-		for(int i=0; i<filter.length() && i<mask.length(); i++) {
-			char f = filter.charAt(i);
-			if (f == '#') continue;
-			
-			char m = mask.charAt(i);
-			if (f!=m) return false;
-			
-		}
-		return true;
-	}
-	
-	public void setState(boolean[] state) {
-		for(int i=0; i<this.state.length && i<state.length; i++) {
-			this.state[i] = state[i];
-		}
-	}
-	
 	public abstract void draw(Canvas canvas);
 	public void destroy(){};
 
--- core/src/fts/graphics/SelectorDrawable.java
+++ core/src/fts/graphics/SelectorDrawable.java
@@ -10,13 +10,16 @@ import fts.core.Application;
 import fts.core.xml.SimpleXML;
 
 public class SelectorDrawable extends Drawable {
+	private static final String filterNames [] = {"selected", "focused", "enabled", "pressed"};
+	private boolean state[] = new boolean[filterNames.length];
+
 	List<String> filters = new ArrayList<String>();
 	List<Drawable> drawables = new ArrayList<Drawable>();
 
 	public void load(Element node) throws JSONException {
 		List<Element> elements = SimpleXML.getElements(node);
 		for(Element element : elements) {
-			filters.add(createFilter(element, new String[] {"selected", "focused", "enabled", "pressed"}));
+			filters.add(createFilter(element, filterNames));
 			drawables.add(Application.createDrawable(SimpleXML.getElement(element, "drawable")));
 		}
 	}
@@ -52,6 +55,36 @@ public class SelectorDrawable extends Drawable {
 		
 		selectedDrawable.setBounds(bounds);
 		selectedDrawable.draw(canvas);
+	}
+
+	public void setState(int index, boolean value) {
+		state[index] = value;
+	}
+
+	public void setState(boolean[] state) {
+		for(int i=0; i<this.state.length && i<state.length; i++) {
+			this.state[i] = state[i];
+		}
+	}
+
+	private String createFilterMask() {
+		String mask = "";
+		for (boolean stateValue : state) {
+			mask = mask + (stateValue?"1":0);
+		}
+		return mask;
+	}
+	
+	private boolean filterMatch(String filter, String mask) {
+		for(int i=0; i<filter.length() && i<mask.length(); i++) {
+			char f = filter.charAt(i);
+			if (f == '#') continue;
+			
+			char m = mask.charAt(i);
+			if (f!=m) return false;
+			
+		}
+		return true;
 	}
 
 	@Override
