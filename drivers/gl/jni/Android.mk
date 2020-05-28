54a9d893399fadf2a3bb4d010393a127421afeac
Franco Catrin <fcatrin@gmail.com>
2020-05-28 14:52:22 -0400

    Build library for android

---
+++ drivers/gl/jni/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := fts_gl_android
+LOCAL_SRC_FILES := fts_gl.cpp graphics.c utils.c libs/nanovg/nanovg.c
+LOCAL_LDLIBS    += -llog -lGLESv2
+
+LOCAL_CFLAGS += -DNANOVG_GLES2_IMPLEMENTATION -DGL_GLEXT_PROTOTYPES 
+LOCAL_CFLAGS += -g -Wall -fPIC -I.
+ifeq ($(TARGET_ARCH),x86)
+#
+else
+	#LOCAL_CFLAGS +=  -marm -O3 -mfpu=neon
+endif 
+include $(BUILD_SHARED_LIBRARY)
+
+
