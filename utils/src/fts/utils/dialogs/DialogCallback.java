23d7ddfa353d0f370d8241b741c3fec32674f90e
Franco Catrin <fcatrin@gmail.com>
2020-10-17 03:28:51 -0300

    fix format

--- utils/src/fts/utils/dialogs/DialogCallback.java
+++ utils/src/fts/utils/dialogs/DialogCallback.java
@@ -1,6 +1,6 @@
 package fts.utils.dialogs;
 
-public abstract class DialogCallback extends SimpleDialogCallback{
+public abstract class DialogCallback extends SimpleDialogCallback {
 
 	public abstract void onYes();
 	public void onNo() {};
