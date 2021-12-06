b2f314e811ec93260ede1be0f17d2db368ffe37f
Franco Catrin <fcatrin@gmail.com>
2021-12-05 23:06:33 -0300

    Extract new StateListSelector class

--- core/src/fts/graphics/SelectorDrawable.java
+++ core/src/fts/graphics/SelectorDrawable.java
@@ -1,6 +1,5 @@
 package fts.graphics;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.w3c.dom.Element;
@@ -9,11 +8,7 @@ import fts.core.Application;
 import fts.core.xml.SimpleXML;
 
 public class SelectorDrawable extends Drawable {
-	private static final String filterNames [] = {"selected", "focused", "enabled", "pressed"};
-	private boolean state[] = new boolean[filterNames.length];
-
-	List<String> filters = new ArrayList<String>();
-	List<Drawable> drawables = new ArrayList<Drawable>();
+	private final StateSelectorDrawable drawableSelector = new StateSelectorDrawable();
 
 	public SelectorDrawable() {}
 	
@@ -21,90 +16,43 @@ public class SelectorDrawable extends Drawable {
 		load(node);
 	}
 	
-	public void load(Element node) {
-		List<Element> elements = SimpleXML.getElements(node, "item");
-		for(Element element : elements) {
-			filters.add(createFilter(element, filterNames));
-			String drawable = SimpleXML.getAttribute(element, "drawable");
-			if (drawable!=null) {
-				drawables.add(Application.loadDrawable(drawable));
-			} else {
-				List<Element> childElements = SimpleXML.getElements(element);
-				if (childElements.size() != 1) throw new RuntimeException("Wrong number of drawable elements on selector " + element);
-				
-				drawables.add(Application.createDrawable(childElements.get(0))); 
-			}
-		}
-	}
-	
-	private String createFilter(Element node, String names[]) {
-		String descriptor = "";
-		for(String name : names) {
-			descriptor = descriptor + createFilter(node, name);
-		}
-		return descriptor;
-	}
-	
-	private String createFilter(Element element, String name) {
-		if (SimpleXML.getAttribute(element, name) == null) return "#";
-		else return SimpleXML.getBoolAttribute(element, name)?"1":"0";
-	}
-	
-	private Drawable selectDrawable() {
-		String mask = createFilterMask();
-		int i=0;
-		for(String filter : filters) {
-			if (filterMatch(filter, mask)) return drawables.get(i);
-			i++;
-		}
-		return drawables.get(drawables.size()-1);
-	}
-	
-
 	@Override
 	public void draw(Canvas canvas) {
-		Drawable selectedDrawable = selectDrawable();
+		Drawable selectedDrawable = drawableSelector.getSelectedItem();
 		if (selectedDrawable == null) return;
 		
 		selectedDrawable.setBounds(bounds);
 		selectedDrawable.draw(canvas);
 	}
-
-	public void setState(int index, boolean value) {
-		state[index] = value;
-	}
-
-	public void setState(boolean[] state) {
-		for(int i=0; i<this.state.length && i<state.length; i++) {
-			this.state[i] = state[i];
-		}
-	}
-
-	private String createFilterMask() {
-		String mask = "";
-		for (boolean stateValue : state) {
-			mask = mask + (stateValue?"1":0);
-		}
-		return mask;
-	}
 	
-	private boolean filterMatch(String filter, String mask) {
-		for(int i=0; i<filter.length() && i<mask.length(); i++) {
-			char f = filter.charAt(i);
-			if (f == '#') continue;
-			
-			char m = mask.charAt(i);
-			if (f!=m) return false;
-			
+	static class StateSelectorDrawable extends StateListSelector<Drawable> {
+
+		@Override
+		protected void destroyItem(Drawable drawable) {
+			drawable.destroy();
 		}
-		return true;
+
+		@Override
+		protected Drawable createItem(Element element) {
+			String drawable = SimpleXML.getAttribute(element, "drawable");
+			if (drawable!=null) {
+				return Application.loadDrawable(drawable);
+			} else {
+				List<Element> childElements = SimpleXML.getElements(element);
+				if (childElements.size() != 1) throw new RuntimeException("Wrong number of drawable elements on selector " + element);
+				
+				return Application.createDrawable(childElements.get(0)); 
+			}
+		}
 	}
 
 	@Override
-	public void destroy() {
-		for(Drawable drawable : drawables) {
-			drawable.destroy();
-		}
+	public void load(Element element) {
+		drawableSelector.load(element);
+	}
+
+	public void setState(int ordinal, boolean value) {
+		drawableSelector.setState(ordinal, value);
 	}
 	
 }
---
+++ core/src/fts/graphics/StateListSelector.java
@@ -0,0 +1,92 @@
+package fts.graphics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Element;
+
+import fts.core.xml.SimpleXML;
+
+public abstract class StateListSelector<T> {
+	private static final String filterNames [] = {"selected", "focused", "enabled", "pressed"};
+	private boolean state[] = new boolean[filterNames.length];
+
+	List<String> filters = new ArrayList<String>();
+	List<T> items = new ArrayList<T>();
+
+	public StateListSelector() {}
+	
+	public StateListSelector(Element node) {
+		load(node);
+	}
+	
+	public void load(Element node) {
+		List<Element> elements = SimpleXML.getElements(node, "item");
+		for(Element element : elements) {
+			filters.add(createFilter(element, filterNames));
+			items.add(createItem(element));
+		}
+	}
+	
+	private String createFilter(Element node, String names[]) {
+		String descriptor = "";
+		for(String name : names) {
+			descriptor = descriptor + createFilter(node, name);
+		}
+		return descriptor;
+	}
+	
+	private String createFilter(Element element, String name) {
+		if (SimpleXML.getAttribute(element, name) == null) return "#";
+		else return SimpleXML.getBoolAttribute(element, name)?"1":"0";
+	}
+	
+	public T getSelectedItem() {
+		String mask = createFilterMask();
+		int i=0;
+		for(String filter : filters) {
+			if (filterMatch(filter, mask)) return items.get(i);
+			i++;
+		}
+		return items.get(items.size()-1);
+	}
+	
+	public void setState(int index, boolean value) {
+		state[index] = value;
+	}
+
+	public void setState(boolean[] state) {
+		for(int i=0; i<this.state.length && i<state.length; i++) {
+			this.state[i] = state[i];
+		}
+	}
+
+	private String createFilterMask() {
+		String mask = "";
+		for (boolean stateValue : state) {
+			mask = mask + (stateValue?"1":0);
+		}
+		return mask;
+	}
+	
+	private boolean filterMatch(String filter, String mask) {
+		for(int i=0; i<filter.length() && i<mask.length(); i++) {
+			char f = filter.charAt(i);
+			if (f == '#') continue;
+			
+			char m = mask.charAt(i);
+			if (f!=m) return false;
+			
+		}
+		return true;
+	}
+
+	public void destroy() {
+		for(T item : items) {
+			destroyItem(item);
+		}
+	}
+	
+	protected abstract void destroyItem(T item);
+	protected abstract T createItem(Element element); 
+}
