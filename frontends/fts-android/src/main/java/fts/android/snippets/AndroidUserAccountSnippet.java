b0fc1065a66ebc5ed37a5671bec6d64d471ef2b6
Franco Catrin <fcatrin@gmail.com>
2022-12-03 15:00:15 -0300

    Add more user account logic into AndroidUserAccountSnippet

--- frontends/fts-android/src/main/java/fts/android/snippets/AndroidUserAccountSnippet.java
+++ frontends/fts-android/src/main/java/fts/android/snippets/AndroidUserAccountSnippet.java
@@ -8,20 +8,36 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import fts.android.AndroidUtils;
 import fts.android.AndroidWindow;
 import fts.android.R;
 import fts.android.Snippet;
 import fts.core.Callback;
+import fts.core.SimpleBackgroundTask;
+import fts.core.SimpleCallback;
 import fts.utils.dialogs.DialogCallback;
+import fts.utils.dialogs.DialogListCallback;
 import fts.utils.dialogs.DialogUtils;
+import fts.utils.dialogs.ListOption;
 
-public class AndroidUserAccountSnippet<T extends AndroidWindow> extends Snippet<T> {
-    private static final int PERMISSIONS_REQUEST = 0x92;
-    private static final int ACCOUNT_PICK = 0x29;
+/*
+    required on main activity:
+    1. Call to requestUserAccount to initiate account selection
+    2. Call this onActivityResult on activity.onActivityResult
+*/
+
+public abstract class AndroidUserAccountSnippet<T extends AndroidWindow> extends Snippet<T> {
+    private static final int PERMISSIONS_REQUEST = 0x9202;
+    private static final int ACCOUNT_PICK = 0x2909;
 
     private Callback<String> permissionsGrantedCallback;
+    private SimpleCallback onAccountSelectedCallback;
+    private boolean allowSelection;
 
     public AndroidUserAccountSnippet(T activity) {
         super(activity);
@@ -105,4 +121,67 @@ public class AndroidUserAccountSnippet<T extends AndroidWindow> extends Snippet<
     public View getRootPanel() {
         return null;
     }
+
+    public void requestUserAccount(String message, SimpleCallback returnCallback, boolean allowSelection) {
+        onAccountSelectedCallback = returnCallback;
+        this.allowSelection = allowSelection;
+
+        checkAccountPermissions(message, new Callback<String>() {
+            @Override
+            public void onResult(String accountName) {
+                proceedWithAccountPermissionsGranted(accountName);
+            }
+        });
+    }
+
+    private void proceedWithAccountPermissionsGranted(String accountName) {
+        if (accountName == null) {
+            List<String> userEmails = AndroidUtils.getGoogleUserEmails(activity);
+            if (userEmails.isEmpty()) {
+                showNoRegisteredAccountsMessage();
+                return;
+            } else if (userEmails.size() > 1 && allowSelection) {
+                openAccountSelection(userEmails);
+                return;
+            } else {
+                accountName = userEmails.get(0);
+            }
+        }
+        proceedWithAccount(accountName);
+    }
+
+    private void openAccountSelection(List<String> accounts) {
+        List<ListOption> options = new ArrayList<>();
+        for(String account : accounts) {
+            options.add(new ListOption(account, account));
+        }
+        DialogUtils.select(activity, options, "Select user account", new DialogListCallback() {
+            @Override
+            public void onItemSelected(String account) {
+                proceedWithAccount(account);
+            }
+        });
+    }
+
+    private void proceedWithAccount(String accountName) {
+        SimpleBackgroundTask task = new SimpleBackgroundTask() {
+            @Override
+            public void onBackgroundTask() throws Exception {
+                registerSelectedAccount(accountName);
+            }
+            @Override
+            public void onSuccess() {
+                onAccountSelectedCallback.onResult();
+            }
+        };
+        task.execute();
+    }
+
+    protected abstract void registerSelectedAccount(String accountName) throws Exception;
+
+    private void showNoRegisteredAccountsMessage() {
+        String msg = getString(R.string.no_user_accounts_on_this_device);
+        DialogUtils.message(activity, msg);
+    }
+
 }
